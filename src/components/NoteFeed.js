86db2bb68bab3b9a69f6259d13ad99ab1354091c
myko <awesomeme155@gmail.com>
2020-05-25 12:58:20 +0300

    feat: render notes feed
    - create note and noteFeed components
    - create a noteFeed query and integrate it with the home component

---
+++ src/components/NoteFeed.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { instanceOf } from 'prop-types';
+
+import Note from './Note';
+
+const NoteFeed = ({ notes }) => (
+  <div>{notes && notes.map((note) => <Note key={note.id} note={note} />)}</div>
+);
+
+NoteFeed.propTypes = {
+  notes: instanceOf(Array).isRequired,
+};
+export default NoteFeed;
