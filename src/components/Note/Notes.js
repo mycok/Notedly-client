b66fdb6230799c5ed28108fed5a09eee1028e077
myko <awesomeme155@gmail.com>
2020-07-14 15:54:35 +0300

    feat: add infinite-scroll functionality
    - install react-infinite-scroll-component library
    - integrate the infinite-scroll component with the Notes component
    - add loadmore functionality to the Notes component

--- src/components/Note/Notes.js
+++ src/components/Note/Notes.js
@@ -1,17 +1,43 @@
 import React from 'react';
 import { useQuery } from '@apollo/client';
+import InfiniteScroll from 'react-infinite-scroll-component';
+import { Spinner, Text } from '@chakra-ui/core';
 
 import { noteFeedQuery } from '../../graphql/queries/noteFeed';
-import NoteFeed from './NoteFeed';
+import SampleNote from './SampleNote';
 import { NotesLoader } from '../shared/Loader';
 import NotFound from '../shared/NotFound';
 import ErrorAlert from '../shared/ErrorAlert';
 import GraphqlErrorHandler from '../shared/GraphqlErrorHandler';
 
 const Notes = () => {
-  const { loading, error, data } = useQuery(noteFeedQuery, {
+  const {
+    loading, error, data, fetchMore,
+  } = useQuery(noteFeedQuery, {
     errorPolicy: 'all',
   });
+
+  const loadMore = () => {
+    fetchMore({
+      query: noteFeedQuery,
+      variables: { cursor: data.noteFeed.cursor },
+      updateQuery: (prevResult, { fetchMoreResult }) => {
+        const prevEntry = prevResult.noteFeed;
+        const newNotes = fetchMoreResult.noteFeed.notes;
+        const newCursor = fetchMoreResult.noteFeed.cursor;
+        const hasNextPage = fetchMoreResult.noteFeed.hasNextPage;
+
+        return {
+          noteFeed: {
+            notes: [...prevEntry.notes, ...newNotes],
+            cursor: newCursor,
+            hasNextPage,
+            __typename: prevEntry.__typename,
+          },
+        };
+      },
+    });
+  };
 
   if (loading) return <NotesLoader backgroundColor="#222121" />;
 
@@ -19,10 +45,30 @@ const Notes = () => {
     return <GraphqlErrorHandler err={error} ErrComponent={ErrorAlert} />;
   }
 
-  return data.noteFeed.notes.length > 0 ? (
-    <NoteFeed notes={data.noteFeed.notes} />
-  ) : (
-    <NotFound size="1000px" />
+  if (data.noteFeed.notes.length === 0) {
+    return <NotFound size="1000px" />;
+  }
+
+  return (
+    <div>
+      <InfiniteScroll
+        height="94vh"
+        dataLength={data.noteFeed.notes.length}
+        next={() => loadMore()}
+        hasMore={data.noteFeed.hasNextPage}
+        loader={<Spinner thickness="4px" color="teal" />}
+        endMessage={(
+          <Text textAlign="center" color="teal.200" fontWeight="md">
+            Yay! You have seen it all
+          </Text>
+        )}
+      >
+        {data
+          && data.noteFeed.notes.map((note) => (
+            <SampleNote key={note.id} note={note} />
+          ))}
+      </InfiniteScroll>
+    </div>
   );
 };
 
